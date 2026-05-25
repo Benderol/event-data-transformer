ec1b9ae7dcf4a4d0d30a4582b5c08da5e920bb35
yatluk_danyil <yatlukdan@gmail.com>
2026-05-25 20:35:33 +0200

    Prototype

---
+++ src/main/java/com/project/event_data_transformer/EventDataTransformerApplication.java
@@ -0,0 +1,17 @@
+package com.project.event_data_transformer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableAsync
+@EnableScheduling
+public class EventDataTransformerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventDataTransformerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/event_data_transformer/EventDataTransformerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.event_data_transformer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDataTransformerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
